1e1f8c31b03e75f400521e5b35a0b9a742430234
Marc Brinkmann <git@marcbrinkmann.de>
2013-12-18 15:08:49 +0100

    Added test for app environment.

---
+++ tests/test_appenv.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask_appconfig import AppConfig
+import pytest
+
+def create_sample_app():
+    app = Flask('testapp')
+    AppConfig(app)
+    return app
+
+
+def test_envmocking(monkeypatch):
+    monkeypatch.setenv('TESTAPP_CONFA', 'a')
+    monkeypatch.setenv('TESTAPP_CONFB', 'b')
+
+    app = create_sample_app()
+    assert app.config['CONFA'] == 'a'
+    assert app.config['CONFB'] == 'b'
