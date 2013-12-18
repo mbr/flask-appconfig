6719e372b74a2cc129dc7d48f3b97cd18c0d8754
Marc Brinkmann <git@marcbrinkmann.de>
2013-12-18 15:16:06 +0100

    Added test for heroku.

---
+++ tests/test_heroku.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+from flask_appconfig import HerokuConfig
+
+
+def create_sample_app():
+    app = Flask('testapp')
+    HerokuConfig(app)
+    return app
+
+
+def test_herokupostgres(monkeypatch):
+    monkeypatch.setenv('HEROKU_POSTGRESQL_ORANGE_URL', 'heroku-db-uri')
+
+    app = create_sample_app()
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == 'heroku-db-uri'
