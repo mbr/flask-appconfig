5feea306f203e7c99e5f60827821a9c756cc9150
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-11 14:35:08 +0200

    Removed unused imports from tests.

--- tests/test_appenv.py
+++ tests/test_appenv.py
@@ -1,8 +1,6 @@
-import os
-
 from flask import Flask
 from flask_appconfig import AppConfig
-import pytest
+
 
 def create_sample_app():
     app = Flask('testapp')
--- tests/test_heroku.py
+++ tests/test_heroku.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 from flask_appconfig import HerokuConfig
 
